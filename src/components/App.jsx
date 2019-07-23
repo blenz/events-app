9e0f18874b584afd368e3e606615a91b0d53d89e
blenz <bbluegga@yahoo.com>
2019-07-22 21:30:34 -0700

    add initial event list and event form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import EventDashboard from './events/EventDashboard';
+import Navbar from './nav/Navbar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <h1>Re-vents</h1>
-      </div>
+        <Navbar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
