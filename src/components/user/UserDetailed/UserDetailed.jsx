1449a789c3f9a35b10d86f89d3aed91be0f05832
blenz <bbluegga@yahoo.com>
2019-07-23 23:14:29 -0700

    add router and pages

---
+++ src/components/user/UserDetailed/UserDetailed.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserDetailed = () => {
+  return (
+    <div>
+      <h1>
+        <h1>User Detailed Page</h1>
+      </h1>
+    </div>
+  );
+};
+
+export default UserDetailed;
