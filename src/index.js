c154a886b76a1b05b5e34eb6abd985b8af30584b
blenz <bbluegga@yahoo.com>
2019-08-14 21:37:12 -0700

    add spacing

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
