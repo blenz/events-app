86a80ffe854084896f92d677f552f792777e1a2f
blenz <bbluegga@yahoo.com>
2019-08-14 21:33:24 -0700

    rename root reducer file

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import rootReducer from './reducers/rootReducer';
+import rootReducer from './reducers';
 import ScrollToTop from './common/util/ScrollToTop';
 import { loadEvent } from './actions/eventActions';
 import ReduxToastr from 'react-redux-toastr';
