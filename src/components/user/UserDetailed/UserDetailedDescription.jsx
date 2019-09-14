9bfa9a8ed6e4ca332bfd428a368944f37882b0a6
blenz <bbluegga@yahoo.com>
2019-09-14 14:53:30 -0700

    add user detail dynamic page

---
+++ src/components/user/UserDetailed/UserDetailedDescription.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Grid, Segment, Item, Header, List, Icon } from 'semantic-ui-react';
+import format from 'date-fns/format';
+
+const UserDetailedDescription = ({ profile }) => {
+  return (
+    <Grid.Column width={12}>
+      <Segment loading={!profile.isLoaded}>
+        <Grid columns={2}>
+          <Grid.Column width={10}>
+            <Header icon='smile' content='About Display Name' />
+            <p>
+              I am a: <strong>{profile.occupation || 'Unknown'}</strong>
+            </p>
+            <p>
+              Originally from <strong>{profile.city}</strong>
+            </p>
+            <p>
+              Member Since:{' '}
+              <strong>
+                {profile.createdAt &&
+                  format(profile.createdAt.toDate(), 'MMM d yyy')}
+              </strong>
+            </p>
+            <p>{profile.description}</p>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Header icon='heart outline' content='Interests' />
+            {profile.interests && profile.interests.length !== 0 ? (
+              <List>
+                {profile.interests.map((interest, i) => (
+                  <Item key={i}>
+                    <Icon name='heart' />
+                    <Item.Content>{interest}</Item.Content>
+                  </Item>
+                ))}
+              </List>
+            ) : (
+              <p>No interests listed</p>
+            )}
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    </Grid.Column>
+  );
+};
+
+export default UserDetailedDescription;
